c278bac364784d8912fdc231768a3961f5ddef18
Grad van Horck <git@gradje.nl>
2013-01-11 21:16:56 +0100

    Initial commit

---
+++ ArduinoComLogger/MainForm.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace ArduinoComLogger
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbCosmStreamId = new System.Windows.Forms.TextBox();
+            this.cbComPort = new System.Windows.Forms.ComboBox();
+            this.tbCosmApiKey = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.minimizeButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.lbLog = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM-poort:";
+            // 
+            // tbCosmStreamId
+            // 
+            this.tbCosmStreamId.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbCosmStreamId.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ArduinoComLogger.Properties.Settings.Default, "CosmListId", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.tbCosmStreamId.Location = new System.Drawing.Point(112, 39);
+            this.tbCosmStreamId.Name = "tbCosmStreamId";
+            this.tbCosmStreamId.Size = new System.Drawing.Size(262, 20);
+            this.tbCosmStreamId.TabIndex = 3;
+            this.tbCosmStreamId.Text = global::ArduinoComLogger.Properties.Settings.Default.CosmListId;
+            // 
+            // cbComPort
+            // 
+            this.cbComPort.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ArduinoComLogger.Properties.Settings.Default, "ComPort", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.cbComPort.FormattingEnabled = true;
+            this.cbComPort.Location = new System.Drawing.Point(112, 12);
+            this.cbComPort.Name = "cbComPort";
+            this.cbComPort.Size = new System.Drawing.Size(121, 21);
+            this.cbComPort.TabIndex = 1;
+            this.cbComPort.Text = global::ArduinoComLogger.Properties.Settings.Default.ComPort;
+            // 
+            // tbCosmApiKey
+            // 
+            this.tbCosmApiKey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbCosmApiKey.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ArduinoComLogger.Properties.Settings.Default, "CostApiKey", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.tbCosmApiKey.Location = new System.Drawing.Point(112, 65);
+            this.tbCosmApiKey.Name = "tbCosmApiKey";
+            this.tbCosmApiKey.Size = new System.Drawing.Size(262, 20);
+            this.tbCosmApiKey.TabIndex = 5;
+            this.tbCosmApiKey.Text = global::ArduinoComLogger.Properties.Settings.Default.CostApiKey;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Stream ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "API Key:";
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.Location = new System.Drawing.Point(299, 280);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 10;
+            this.closeButton.Text = "Sluiten";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // minimizeButton
+            // 
+            this.minimizeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.minimizeButton.Location = new System.Drawing.Point(207, 280);
+            this.minimizeButton.Name = "minimizeButton";
+            this.minimizeButton.Size = new System.Drawing.Size(86, 23);
+            this.minimizeButton.TabIndex = 9;
+            this.minimizeButton.Text = "Minimaliseren";
+            this.minimizeButton.UseVisualStyleBackColor = true;
+            this.minimizeButton.Click += new System.EventHandler(this.minimizeButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(112, 91);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(193, 91);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 7;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+            // 
+            // lbLog
+            // 
+            this.lbLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbLog.FormattingEnabled = true;
+            this.lbLog.Location = new System.Drawing.Point(12, 121);
+            this.lbLog.Name = "lbLog";
+            this.lbLog.Size = new System.Drawing.Size(362, 147);
+            this.lbLog.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 315);
+            this.Controls.Add(this.lbLog);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.minimizeButton);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbCosmApiKey);
+            this.Controls.Add(this.cbComPort);
+            this.Controls.Add(this.tbCosmStreamId);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(296, 325);
+            this.Name = "MainForm";
+            this.Text = "Arduino Logger";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbCosmStreamId;
+        private System.Windows.Forms.ComboBox cbComPort;
+        private System.Windows.Forms.TextBox tbCosmApiKey;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button minimizeButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ListBox lbLog;
+    }
+}
+
---
+++ ArduinoComLogger/MainForm.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.Net;
+
+namespace ArduinoComLogger
+{
+    public partial class MainForm : Form
+    {
+        Thread readThread;
+        static bool _continue;
+        static System.IO.Ports.SerialPort serialPort;
+        static string _apiKey;
+        static string _listId;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            ArduinoComLogger.Properties.Settings.Default.Save();
+
+            Application.Exit();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.cbComPort.Items.AddRange(SerialPort.GetPortNames());
+            serialPort = new SerialPort();
+        }
+
+        private void minimizeButton_Click(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            this.Hide();
+        }
+
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = true;
+            this.Show();
+        }
+
+        protected void setState(bool enabled)
+        {
+            this.cbComPort.Enabled =
+                this.tbCosmApiKey.Enabled =
+                this.tbCosmStreamId.Enabled =
+                    !enabled;
+
+            this.startButton.Enabled = !enabled;
+            this.stopButton.Enabled = enabled;
+        }
+
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            setState(true);
+            readThread = new Thread(ReadSerialPort);
+
+            serialPort.PortName = cbComPort.Text;
+            serialPort.ReadTimeout = 500;
+            serialPort.WriteTimeout = 500;
+
+            _apiKey = tbCosmApiKey.Text;
+            _listId = tbCosmStreamId.Text;
+
+            //var values = new string[] { "200", "300", "400" };
+            //PostValue(values);
+
+            serialPort.Open();
+            _continue = true;
+            readThread.Start(this);
+
+            AddToLog("Started listening on " + cbComPort.Text);
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            AddToLog("Closing connection... ");
+            _continue = false;
+            readThread.Join();
+            serialPort.Close();
+            AddToLog("Connection closed");
+
+            setState(false);
+        }
+
+        public static void ReadSerialPort(object data)
+        {
+            MainForm form = (MainForm)data;
+
+            while (_continue)
+            {
+                try
+                {
+                    string message = serialPort.ReadLine();
+                    form.AddToLog(message);
+                    if (message[0] == '~') {
+                        message = message.TrimStart('~');
+                        PostValue(message);
+                    }
+                }
+                catch (TimeoutException) { }
+            }
+        }
+
+        public static void PostValue(string[] values)
+        {
+            PostValue(String.Join(",", values));
+        }
+
+
+        public static void PostValue(string value)
+        {
+             byte[] postArray = Encoding.ASCII.GetBytes(value);
+            WebClient wc = new WebClient();
+            wc.Headers.Add("X-PachubeApiKey", _apiKey);
+            wc.UploadData("http://www.pachube.com/api/" + _listId + ".csv", "PUT", postArray);
+        }
+
+        delegate void AddToLogWindowDelegate(String msg);
+
+        public void AddToLog(String message)
+        {
+            // Check whether the caller must call an invoke method when making method calls to listBoxCCNetOutput because the caller is 
+            // on a different thread than the one the listBoxCCNetOutput control was created on.
+            if (lbLog.InvokeRequired)
+            {
+                AddToLogWindowDelegate update = new AddToLogWindowDelegate(AddToLog);
+                lbLog.Invoke(update, message);
+            }
+            else
+            {
+                lbLog.Items.Add(message);
+                if (lbLog.Items.Count > 50)
+                {
+                    lbLog.Items.RemoveAt(0); // remove first line
+                }
+                // Make sure the last item is made visible
+                lbLog.SelectedIndex = lbLog.Items.Count - 1;
+                lbLog.ClearSelected();
+            }
+        }
+    }
+}
